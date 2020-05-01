370f429eecc61ff86e957f68a5b42c0425d22908
Maxim Rietveld <maxrietveld@gmail.com>
2020-05-01 10:15:54 -0700

    instantiated server/index.js, configed webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,22 @@
-
+const path = require('path');
+const src_dir = path.join(__dirname + '/client/');
+const dis_dir = path.join(__dirname + '/public');
 
 module.export = {
-  entry:
+  entry: `${src_dir}/index.js`,
+  output: {
+    path: dis_dir,
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?/,
+        include: src_dir,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015']
+      }
+    ]
+  }
 }
