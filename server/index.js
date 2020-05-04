68489d7dd9199a2de487a67a4a89bf2a8d7ebc7b
Maxim Rietveld <maxrietveld@gmail.com>
2020-05-04 10:27:52 -0700

    eslint run on every file with all errors resolved, moving on to Jest/Enzyme

--- server/index.js
+++ server/index.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const {getRoomData} = require ('./routeHandlers.js');
+const { getRoomData } = require('./routeHandlers.js');
 
 const app = express();
 
 const PORT = 3000;
-app.listen(PORT, ()=>{console.log(`Express server listening on port#${PORT}`)});
+app.listen(PORT, () => { console.log(`Express server listening on port#${PORT}`); });
 
 app.use(express.static('public'));
 
