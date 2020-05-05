b2bc53797c2cdc6c5a3d63e69d47e254073d949e
Maxim Rietveld <maxrietveld@gmail.com>
2020-05-05 15:15:11 -0700

    completed eslint, some unresolved errors to fix

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import MainCarousel from './MainCarousel.jsx';
 import Axios from 'axios';
+import MainCarousel from './MainCarousel.jsx';
 
 class App extends React.Component {
   constructor(props) {
@@ -9,36 +9,36 @@ class App extends React.Component {
     this.state = {
       data: [
         {
-          "room_id": 1,
-          "host_id": 1,
-          "location": "Nasvhille, TN",
-          "rating": 4.4,
-          "room_type": "Mansion",
-          "bed_num": 6,
-          "description": "A frat boys dream come true.",
-          "price_per_night": 60,
-          "img_src": [
-            "https://loremflickr.com/cache/resized/65535_49259266533_6deda510e9_320_240_nofilter.jpg",
-            "https://loremflickr.com/cache/resized/65535_49259266533_6deda510e9_320_240_nofilter.jpg",
-            "https://loremflickr.com/cache/resized/65535_49259266533_6deda510e9_320_240_nofilter.jpg"
-          ]
+          room_id: 1,
+          host_id: 1,
+          location: 'Nasvhille, TN',
+          rating: 4.4,
+          room_type: 'Mansion',
+          bed_num: 6,
+          description: 'A frat boys dream come true.',
+          price_per_night: 60,
+          img_src: [
+            'https://loremflickr.com/cache/resized/65535_49259266533_6deda510e9_320_240_nofilter.jpg',
+            'https://loremflickr.com/cache/resized/65535_49259266533_6deda510e9_320_240_nofilter.jpg',
+            'https://loremflickr.com/cache/resized/65535_49259266533_6deda510e9_320_240_nofilter.jpg',
+          ],
         },
         {
-          "room_id": 2,
-          "host_id": 1,
-          "location": "Nashville, TN",
-          "rating": 4.7,
-          "room_type": "Towers",
-          "bed_num": 24,
-          "description": "Another frat boys dream come true.",
-          "price_per_night": 25,
-          "img_src": [
-            "https://loremflickr.com/cache/resized/65535_47722650711_107f705e93_320_240_nofilter.jpg",
-            "https://loremflickr.com/cache/resized/65535_47722650711_107f705e93_320_240_nofilter.jpg",
-            "https://loremflickr.com/cache/resized/65535_47722650711_107f705e93_320_240_nofilter.jpg"
-          ]
-        }
-      ]
+          room_id: 2,
+          host_id: 1,
+          location: 'Nashville, TN',
+          rating: 4.7,
+          room_type: 'Towers',
+          bed_num: 24,
+          description: 'Another frat boys dream come true.',
+          price_per_night: 25,
+          img_src: [
+            'https://loremflickr.com/cache/resized/65535_47722650711_107f705e93_320_240_nofilter.jpg',
+            'https://loremflickr.com/cache/resized/65535_47722650711_107f705e93_320_240_nofilter.jpg',
+            'https://loremflickr.com/cache/resized/65535_47722650711_107f705e93_320_240_nofilter.jpg',
+          ],
+        },
+      ],
     };
   }
 
@@ -47,25 +47,24 @@ class App extends React.Component {
       .then((results) => {
         console.log(results.data);
         this.setState({
-          data: results.data
-        })
+          data: results.data,
+        });
       })
-      .catch((err) => {console.error(err)})
+      .catch((err) => { console.error(err); });
   }
 
   render() {
     if (this.state.data.length === 0) {
       return (
-        <div className='loading_div'>Loading data, please wait.</div>
-      )
-    } else {
-      return (
-        <div>
-          <h2 className='componentTitle'>More homes you may like</h2>
-          <MainCarousel data={this.state.data}/>
-        </div>
+        <div className="loading_div">Loading data, please wait.</div>
       );
     }
+    return (
+      <div>
+        <h2 className="componentTitle">More homes you may like</h2>
+        <MainCarousel data={this.state.data} />
+      </div>
+    );
   }
 }
 
--- client/components/mainCarousel.jsx
+++ client/components/mainCarousel.jsx
@@ -5,53 +5,55 @@ import MainCarArrows from './MainCarArrows.jsx';
 class MainCarousel extends React.Component {
   constructor(props) {
     super(props);
-    this.state={
-      currentElementIndex: 0
-    }
+    this.state = {
+      currentElementIndex: 0,
+    };
     this.previousElement = this.previousElement.bind(this);
     this.nextElement = this.nextElement.bind(this);
   }
 
   previousElement() {
     const lastIndex = this.props.data.length - 1;
-    const {currentElementIndex} = this.state;
+    const { currentElementIndex } = this.state;
     const shouldResetIndex = currentElementIndex === 0;
     const index = shouldResetIndex ? lastIndex : currentElementIndex - 1;
 
     this.setState({
-      currentElementIndex: index
-    })
+      currentElementIndex: index,
+    });
   }
 
   nextElement() {
     const lastIndex = this.props.data.length - 1;
-    const {currentElementIndex} = this.state;
+    const { currentElementIndex } = this.state;
     const shouldResetIndex = currentElementIndex === lastIndex;
     const index = shouldResetIndex ? 0 : currentElementIndex + 1;
 
     this.setState({
-      currentElementIndex: index
-    })
+      currentElementIndex: index,
+    });
   }
 
   render() {
     return (
-      <div className='mainCarousel'>
+      <div className="mainCarousel">
 
         <MainCarArrows
           direction="left"
           clickFunction={this.previousElement}
-          glyph="&#9664;"/>
+          glyph="&#9664;"
+        />
 
-        <MainCarElement data={this.props.data} currentElement={this.state.currentElementIndex}/>
+        <MainCarElement data={this.props.data} currentElement={this.state.currentElementIndex} />
 
         <MainCarArrows
           direction="right"
           clickFunction={this.nextElement}
-          glyph="&#9654;"/>
+          glyph="&#9654;"
+        />
 
       </div>
-    )
+    );
   }
 }
 
