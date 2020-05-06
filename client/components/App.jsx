2b0eca051f5a99165bdd0bed4003108b50fb8866
Maxim Rietveld <maxrietveld@gmail.com>
2020-05-06 11:15:14 -0700

    reconfiged database sampledata to be more clear cut

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -7,38 +7,7 @@ class App extends React.Component {
     super(props);
 
     this.state = {
-      data: [
-        {
-          room_id: 1,
-          host_id: 1,
-          location: 'Nasvhille, TN',
-          rating: 4.4,
-          room_type: 'Mansion',
-          bed_num: 6,
-          description: 'A frat boys dream come true.',
-          price_per_night: 60,
-          img_src: [
-            'https://loremflickr.com/cache/resized/65535_49259266533_6deda510e9_320_240_nofilter.jpg',
-            'https://loremflickr.com/cache/resized/65535_49259266533_6deda510e9_320_240_nofilter.jpg',
-            'https://loremflickr.com/cache/resized/65535_49259266533_6deda510e9_320_240_nofilter.jpg',
-          ],
-        },
-        {
-          room_id: 2,
-          host_id: 1,
-          location: 'Nashville, TN',
-          rating: 4.7,
-          room_type: 'Towers',
-          bed_num: 24,
-          description: 'Another frat boys dream come true.',
-          price_per_night: 25,
-          img_src: [
-            'https://loremflickr.com/cache/resized/65535_47722650711_107f705e93_320_240_nofilter.jpg',
-            'https://loremflickr.com/cache/resized/65535_47722650711_107f705e93_320_240_nofilter.jpg',
-            'https://loremflickr.com/cache/resized/65535_47722650711_107f705e93_320_240_nofilter.jpg',
-          ],
-        },
-      ],
+      data: []
     };
   }
 
