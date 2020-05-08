29c418f411cd55b929a17ea3a9de221864a77653
Maxim Rietveld <maxrietveld@gmail.com>
2020-05-08 14:11:05 -0700

    inner carousel taking shape, images being produced properly. Building buttons now

--- client/components/elementCarousel.jsx
+++ client/components/elementCarousel.jsx
@@ -1,19 +1,41 @@
 import React from 'react';
 import styled from 'styled-components';
 
+const MHYML_EC_E_Wrapper = styled.div`
+  overflow: hidden !important;
+  width: 333px;
+  height: 222px;
+  box-sizing: border-box;
+  display: block;
+`;
+
+const MHYML_Transformer = styled.div`
+  margin-bottom: 0px !important;
+  padding: 0px !important;
+  overflow: visible !important;
+  white-space: nowrap !important;
+  transition: -ms-transform 0.5s ease 0s, -webkit-transform 0.5s ease 0s, transform 0.5s ease 0s !important;
+  transform: translateX(${(props) => props.state}%);
+`;
+
 class ElementCarousel extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
       imgData: this.props.images,
-      translateX: 0
+      translateX: -50
     }
   }
 
   render() {
     return (
-      <img src={this.state.imgData[0]} alt='houses'></img>
+      <MHYML_EC_E_Wrapper>
+        <MHYML_Transformer state={this.state.translateX}>
+          {this.state.imgData.map(image => <img src={image} alt='houses' key={image}></img>)}
+        </MHYML_Transformer>
+      </MHYML_EC_E_Wrapper>
+
     )
   }
 }
