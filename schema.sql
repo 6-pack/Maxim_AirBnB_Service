9e080316fabfa81ce91628f31d29d833b5c38c8b
Maxim Rietveld <maxrietveld@gmail.com>
2020-05-08 11:58:01 -0700

    beginning work on inner carousel

--- schema.sql
+++ schema.sql
@@ -24,8 +24,8 @@ CREATE TABLE roomTable (
 INSERT INTO hostTable (name) VALUES ('host1');
 INSERT INTO hostTable (name) VALUES ('host2');
 
-INSERT INTO roomTable (host_id, location, rating, room_type, bed_num, description, price_per_night, img_src) VALUES (1, 'Nasvhille, TN', 4.4, 'Mansion', 6, 'A frat boys dream come true.', 60, '{https://loremflickr.com/cache/resized/65535_49794168986_1e749bbbd9_320_240_nofilter.jpg}');
-INSERT INTO roomTable (host_id, location, rating, room_type, bed_num, description, price_per_night, img_src) VALUES (1, 'Nashville, TN', 4.7, 'Towers', 24, 'Another frat boys dream come true.', 25, '{https://loremflickr.com/cache/resized/65535_49661753357_b6329f521f_320_240_nofilter.jpg}');
-INSERT INTO roomTable (host_id, location, rating, room_type, bed_num, description, price_per_night, img_src) VALUES (2, 'Nashville, TN', 4.1, 'Brandscomb', 320, 'A grimey Vanderbilt experience.', 35, '{https://loremflickr.com/cache/resized/65535_49792393086_a117e3bc38_320_240_nofilter.jpg}');
+INSERT INTO roomTable (host_id, location, rating, room_type, bed_num, description, price_per_night, img_src) VALUES (1, 'Nasvhille, TN', 4.4, 'Mansion', 6, 'A frat boys dream come true.', 60, '{https://loremflickr.com/cache/resized/65535_49602779156_c98f9c5a42_333_222_nofilter.jpg, https://loremflickr.com/cache/resized/65535_49573641653_ddb5a1b477_333_222_nofilter.jpg, https://loremflickr.com/cache/resized/65535_49604844007_b182a2b4d4_333_222_nofilter.jpg}');
+INSERT INTO roomTable (host_id, location, rating, room_type, bed_num, description, price_per_night, img_src) VALUES (1, 'Nashville, TN', 4.7, 'Towers', 24, 'Another frat boys dream come true.', 25, '{https://loremflickr.com/cache/resized/65535_49407311648_b232eebbf3_333_222_nofilter.jpg}');
+INSERT INTO roomTable (host_id, location, rating, room_type, bed_num, description, price_per_night, img_src) VALUES (2, 'Nashville, TN', 4.1, 'Brandscomb', 320, 'A grimey Vanderbilt experience.', 35, '{https://loremflickr.com/cache/resized/65535_49503060198_bbc21c2063_333_222_nofilter.jpg}');
 
 --caution. Images up to this point do not have proper photo renderings. More like joke setups...
