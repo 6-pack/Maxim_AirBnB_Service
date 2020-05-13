7ea9286c050b54abcf7c725a0f0335c62a3bb460
Maxim Rietveld <maxrietveld@gmail.com>
2020-05-13 15:58:54 -0700

    got dockerized rendering working with db, now developing docker-compose

--- db/pg.config.js
+++ db/pg.config.js
@@ -1,7 +1,8 @@
 module.exports = {
   dbConfig: {
     user: 'postgres',
-    host: 'localhost',
+    password: 'postgres',
+    host: '172.17.0.2',
     database: 'airbnb_mhyml',
     port: 5432
   }
