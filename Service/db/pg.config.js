01a969b30c09fd43a79b449a8b412aa07ab84eb1
Maxim Rietveld <maxrietveld@gmail.com>
2020-05-14 10:27:57 -0700

    Refactored to use .yml

--- Service/db/pg.config.js
+++ Service/db/pg.config.js
@@ -2,7 +2,7 @@ module.exports = {
   dbConfig: {
     user: 'postgres',
     password: 'postgres',
-    host: '172.17.0.2',
+    host: 'database_maxim',
     database: 'airbnb_mhyml',
     port: 5432
   }
