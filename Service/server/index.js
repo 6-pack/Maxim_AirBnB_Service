9857b5af0ae14f6db8a0a32862a1584994613b60
Maxim Rietveld <maxrietveld@gmail.com>
2020-05-15 15:47:03 -0700

    setup webpack-compression-plugin, time to test

--- Service/server/index.js
+++ Service/server/index.js
@@ -7,6 +7,11 @@ const PORT = 3004;
 app.listen(PORT, () => { console.log(`Express server listening on port#${PORT}`); });
 
 app.use(express.static('public'));
+app.get('*.js', function (req, res, next) {
+  req.url = req.url + '.gz';
+  res.set('Content-Encoding', 'gzip');
+  next();
+});
 
 // routes
 app.get('/rooms', getRoomData);
--- Service/webpack.config.js
+++ Service/webpack.config.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const src_dir = path.join(__dirname + '/client');
 const dist_dir = path.join(__dirname + '/public');
+const CompressionPlugin = require('compression-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
   entry: `${src_dir}/index.jsx`,
@@ -21,5 +23,22 @@ module.exports = {
         }
       }
     ]
-  }
+  },
+  plugins: [
+    new webpack.DefinePlugin({ //<--key to reduce React's size
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.UglifyJsPlugin(),
+    new webpack.optimize.AggressiveMergingPlugin(),
+    new CompressionPlugin({
+      asset: "[path].gz[query]",
+      algorithm: "gzip",
+      test: /\.js$|\.css$|\.html$/,
+      threshold: 10240,
+      minRatio: 0.8
+    })
+  ],
 }
