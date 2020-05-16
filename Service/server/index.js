3904d36ece2bf2feb2c794c58403693eb2577bed
Maxim Rietveld <maxrietveld@gmail.com>
2020-05-16 10:52:40 -0700

    serving compressed file, potentially fixed proxy interactions

--- Service/server/index.js
+++ Service/server/index.js
@@ -7,13 +7,7 @@ const app = express();
 const PORT = 3004;
 app.listen(PORT, () => { console.log(`Express server listening on port#${PORT}`); });
 
-app.use(express.static('public'));
-app.use('/', expressStaticGzip('../public'), {
-  customCompressions: [{
-    encodingName: 'deflate',
-    fileExtension: 'gz'
-  }]
-}));
+app.use('/', expressStaticGzip('public'));
 
 // routes
 app.get('/rooms', getRoomData);
