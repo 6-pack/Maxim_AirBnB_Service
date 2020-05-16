bea2bd0043e0387e74d49e98664e7fd20b125222
Maxim Rietveld <maxrietveld@gmail.com>
2020-05-16 10:56:12 -0700

    refactored host for db connection

--- Service/db/pg.config.js
+++ Service/db/pg.config.js
@@ -2,7 +2,7 @@ module.exports = {
   dbConfig: {
     user: 'postgres',
     password: 'postgres',
-    host: 'localhost',
+    host: 'database_maxim',
     database: 'airbnb_mhyml',
     port: 5432
   }
