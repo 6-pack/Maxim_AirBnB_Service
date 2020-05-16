81e9d33059b5ce45bd4c81e77b3991cb2d990325
Maxim Rietveld <maxrietveld@gmail.com>
2020-05-16 10:30:21 -0700

    added middleware

--- Service/server/index.js
+++ Service/server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { getRoomData } = require('./routeHandlers.js');
+const expressStaticGzip = require('express-static-gzip')
 
 const app = express();
 
@@ -7,11 +8,12 @@ const PORT = 3004;
 app.listen(PORT, () => { console.log(`Express server listening on port#${PORT}`); });
 
 app.use(express.static('public'));
-app.get('*.js', function (req, res, next) {
-  req.url = req.url + '.gz';
-  res.set('Content-Encoding', 'gzip');
-  next();
-});
+app.use('/', expressStaticGzip('../public'), {
+  customCompressions: [{
+    encodingName: 'deflate',
+    fileExtension: 'gz'
+  }]
+}));
 
 // routes
 app.get('/rooms', getRoomData);
